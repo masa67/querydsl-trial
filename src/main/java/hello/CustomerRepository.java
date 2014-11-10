7cc080f30727b47d2023b66d3f5325fe596e4778
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame9-THINK>
2014-11-10 14:58:43 +0200

    v.1.2 - QCustomer cannot be found.

--- src/main/java/hello/CustomerRepository.java
+++ src/main/java/hello/CustomerRepository.java
@@ -6,13 +6,15 @@
 package hello;
 
 import java.util.List;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  *
  * @author Ohjelmistokehitys
  */
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends CrudRepository<Customer, Long>,
+    QueryDslPredicateExecutor<Customer> {
     
     List<Customer> findByLastName(String lastName);
 }
