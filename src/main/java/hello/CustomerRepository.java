c0f12d09b18ca128a4b674d07e32472badce2537
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame9-THINK>
2014-11-10 13:54:58 +0200

    v.1.0 - The initial working example.

---
+++ src/main/java/hello/CustomerRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Ohjelmistokehitys
+ */
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    
+    List<Customer> findByLastName(String lastName);
+}
